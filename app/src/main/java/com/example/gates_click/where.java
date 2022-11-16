fa145be6bbd0012686ba2c563964fcbf318be83e
kimchingtiu <kimtiu00@gmail.com>
2022-11-16 16:43:15 +0800

    gates

---
+++ app/src/main/java/com/example/gates_click/where.java
@@ -0,0 +1,14 @@
+package com.example.gates_click;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class where extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_where);
+    }
+}
